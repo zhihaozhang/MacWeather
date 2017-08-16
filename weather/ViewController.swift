f2a2c387efa8632480c40c9cd44adf039492fa52
Chih-Hao <zhihaozhang@me.com>
2017-08-16 20:43:45 +0800

    Initial Commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Chih-Hao on 2017/8/16.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
